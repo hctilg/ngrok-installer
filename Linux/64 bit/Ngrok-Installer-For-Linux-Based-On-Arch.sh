56b686c3d3d87c8cd4ed5e1932c9df7eb515532e
♪♪ Mahi <98208009+hctilg@users.noreply.github.com>
2024-03-04 14:50:52 +0330

    Update Ngrok-Installer-For-Linux-Based-On-Arch.sh

--- Linux/64 bit/Ngrok-Installer-For-Linux-Based-On-Arch.sh
+++ Linux/64 bit/Ngrok-Installer-For-Linux-Based-On-Arch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo sudo pacman -Syu
-sudo pacman -S wget zip
+yes | sudo sudo pacman -Syu
+yes | sudo pacman -S wget zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ~/ngrok.zip
 sudo unzip ~/ngrok.zip -d /usr/bin 
 rm -rf ~/ngrok.zip
