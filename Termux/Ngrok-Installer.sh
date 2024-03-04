ef2bf11c530deffe5b2697a4d5b5e6779b466c43
♪♪ Mahi <98208009+hctilg@users.noreply.github.com>
2024-03-04 14:56:02 +0330

    Update Ngrok-Installer.sh

--- Termux/Ngrok-Installer.sh
+++ Termux/Ngrok-Installer.sh
@@ -2,7 +2,7 @@
 
 apt update && apt full-upgrade -y
 apt install wget zip -y
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -O ~/ngrok.zip
+wget -c "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip" -O ~/ngrok.zip
 unzip ~/ngrok.zip -d $PREFIX/bin
 rm -rf ~/ngrok.zip
 
